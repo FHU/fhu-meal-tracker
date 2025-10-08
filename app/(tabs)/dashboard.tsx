b8381c1226931907fbb54b8b2168135e6dded9ed
Kenan Casey <kenancasey@gmail.com>
2025-10-08 10:09:11 -0500

    add fetch

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
 import ProgressBar from "@/components/ProgressBar";
 import { Text, View } from "@/components/Themed";
 
 export default function DashboardScreen() {
+  const getData = async () => {
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+    setMealsRemaining(data.meals.remaining);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const [mealsRemaining, setMealsRemaining] = useState(0);
+
+  // use the data as js object
+  // some state = data.meals.remaining
+
   const stepsToday = 8642;
   const stepsGoal = 10000;
 
@@ -55,12 +72,15 @@ export default function DashboardScreen() {
 
       <ProgressBar
         title={`Savings Account`}
-        label={`$${savingsBalance} / ${savingsGoal} as of ${new Date().toLocaleDateString("en-US", {
-              weekday: "short", // "Monday"
-              year: "numeric", // "2025"
-              month: "short", // "September"
-              day: "numeric", // "29"
-            })}`}
+        label={`$${savingsBalance} / ${savingsGoal} as of ${new Date().toLocaleDateString(
+          "en-US",
+          {
+            weekday: "short", // "Monday"
+            year: "numeric", // "2025"
+            month: "short", // "September"
+            day: "numeric", // "29"
+          }
+        )}`}
         percentage={(savingsBalance / savingsGoal) * 100}
       />
     </View>
